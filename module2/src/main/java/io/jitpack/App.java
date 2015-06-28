c512af7a97dc67b5822d34ba0e306cfb6bd4f288
JitPack.io <jitpack@jitpack.io>
2015-06-28 14:38:50 +0100

    Update App.java

--- module2/src/main/java/io/jitpack/App.java
+++ module2/src/main/java/io/jitpack/App.java
@@ -6,8 +6,10 @@ package io.jitpack;
  */
 public class App 
 {
+    public static String GREETING = "Hello World!";
+    
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( GREETING );
     }
 }
